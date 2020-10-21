007931db343c3725f3778a20db7551854dff306f
tristan-da-silva <tristan.dasilvasousa@gmail.com>
2020-10-21 09:59:42 +0200

    update front

--- episen-sca-tda-front/src/app/components/user/user.component.ts
+++ episen-sca-tda-front/src/app/components/user/user.component.ts
@@ -5,7 +5,7 @@ import { User } from 'src/app/models/user.model';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
 
